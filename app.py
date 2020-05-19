8a9fdf7b95673d8569c672c9775d0dc4f51ad9bf
Sohit Miglani <sohitmiglani@gmail.com>
2020-05-18 21:44:34 -0700

    updated alerts

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # importing general extensions of flask here
 from flask import Flask, session, render_template, request, flash, url_for, redirect
 import flask
+from flask_cors import CORS
 import pandas as pd
 import app_functions
 import os
@@ -9,7 +10,10 @@ import os
 # You can jump directly to building your functions, and collecting HTML inputs for processing.
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 app.config["SECRET_KEY"] = app_functions.random_id(50)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -72,8 +76,7 @@ def health():
                             bmi = session['bmi'],
                             name = session['name'],
                             current_status = 'caution',
-                            heart_rate_status = 'Warning',
-                            pulse_rate_status = 'Warning')
+                            heart_rate_status = 'caution')
 
 @app.after_request
 def add_header(r):
